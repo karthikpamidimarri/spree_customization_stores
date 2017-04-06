9f707a9febbbcac4222f7961acc3ab590a4335ef
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-04-06 21:50:30 +0530

    If session empty continue Shopping button will redirect to home/suppliers page

--- app/overrides/spree/orders/update_product_path_store_path.rb
+++ app/overrides/spree/orders/update_product_path_store_path.rb
@@ -3,7 +3,8 @@ Deface::Override.new(virtual_path: 'spree/orders/edit',
   replace: '[data-hook="empty_cart"]',
   text: "
     <div class='alert alert-info'><%= Spree.t(:your_cart_is_empty) %></div>
-    <p><%= link_to Spree.t(:continue_shopping), spree.supplier_path(session[:supplier_id]), class: 'btn btn-default' %></p>
+    <% if session[:supplier_id] != nil %><p><%= link_to Spree.t(:continue_shopping), spree.supplier_path(session[:supplier_id]), class: 'btn btn-default' %></p><% else %>
+    <p><%= link_to Spree.t(:continue_shopping), spree.root_path, class: 'btn btn-default' %></p><% end %>
   ")
 Deface::Override.new(virtual_path: 'spree/orders/edit',
                      name: 'update_product_path_store_path',
@@ -12,6 +13,7 @@ Deface::Override.new(virtual_path: 'spree/orders/edit',
  <p id='clear_cart_link' data-hook>
           <%= submit_tag Spree.t(:empty_cart), class: 'btn btn-default' %>
           <%= Spree.t(:or) %>
-          <%= link_to Spree.t(:continue_shopping), spree.supplier_path(session[:supplier_id]), class: 'continue' %>
+          <% if session[:supplier_id] != nil %><%= link_to Spree.t(:continue_shopping), spree.supplier_path(session[:supplier_id]), class: 'continue' %><% else %>
+          <%= link_to Spree.t(:continue_shopping), spree.root_path, class: 'continue' %><% end %>
         </p>
   ")
